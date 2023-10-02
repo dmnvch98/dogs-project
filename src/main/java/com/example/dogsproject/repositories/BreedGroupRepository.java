8d6e268d1888421ec8578f4268329911fa0d1aaa
dmn98 <dmnvch.evg@gmail.com>
2023-10-02 16:11:48 +0400

    added transactions when saving breed group and logging of transactions

--- src/main/java/com/example/dogsproject/repositories/BreedGroupRepository.java
+++ src/main/java/com/example/dogsproject/repositories/BreedGroupRepository.java
@@ -3,5 +3,7 @@ package com.example.dogsproject.repositories;
 import com.example.dogsproject.models.BreedGroup;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BreedGroupRepository extends CrudRepository<BreedGroup, Long> {
 }
--- src/main/java/com/example/dogsproject/services/BreedService.java
+++ src/main/java/com/example/dogsproject/services/BreedService.java
@@ -10,9 +10,11 @@ import com.example.dogsproject.repositories.BreedRepository;
 import jakarta.annotation.PostConstruct;
 import kong.unirest.GenericType;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.List;
@@ -20,6 +22,7 @@ import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class BreedService {
     private final BreedGroupRepository breedGroupRepository;
     private final ApiSender apiSender;
@@ -30,10 +33,18 @@ public class BreedService {
 
     @PostConstruct
     private void fetchBreedsFromRemoteRepositoryAndSave() {
+        log.info("Getting breeds from remote repository");
         BreedDto breedDto = getBreedsFromRemoteRepository();
 
         List<BreedGroup> breedGroupList = parseBreedGroupsFromBreedDto(breedDto);
 
+        log.info("Breeds were fetched and parsed. The breedGroupList size is {}", breedGroupList.size());
+
+        saveAll(breedGroupList);
+    }
+
+    @Transactional
+    private void saveAll(List<BreedGroup> breedGroupList) {
         breedGroupRepository.saveAll(breedGroupList);
     }
 
